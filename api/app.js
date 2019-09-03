4ef9f99cc5d0635187b4e394f152d9325c5255b0
Greg Dillon <gregdillon@gmail.com>
2019-09-03 11:06:20 -0400

    Start/not working

--- api/app.js
+++ api/app.js
@@ -7,7 +7,9 @@ var cors = require("cors");
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
+//Test Code
 var testAPIRouter = require("./routes/testAPI");
+var testDBRouter = require("./routes/testDB");
 
 var app = express();
 
@@ -24,6 +26,8 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
+//Test Routes
+app.use("/testDB", testDBRouter);
 app.use("/testAPI", testAPIRouter);
 
 // catch 404 and forward to error handler
